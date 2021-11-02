0e18ef3e449380bc8ed43b9d6f3c42aa3cb2f057
Argus Waikhom <realarguswaikhom@gmail.com>
2021-11-02 22:54:07 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/biblog/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.biblog
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Biblog',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const Center(child: Text('Biblog')),
+    );
+  }
+}
